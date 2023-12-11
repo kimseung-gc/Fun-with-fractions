f1a1f3823b71a30573cde3d46d397b6ca2127e34
Seunghyeon Kim <seunghk1206@gmail.com>
2023-12-11 00:35:34 -0600

    Update InteractiveCalculator.java

--- InteractiveCalculator.java
+++ InteractiveCalculator.java
@@ -10,16 +10,16 @@ import java.io.PrintWriter;
 
 public class InteractiveCalculator {
   public static void main(String[] args) throws Exception {
-    /* Initialization of variables and classes */
+    // Initialization of variables and classes
     PrintWriter pen = new PrintWriter(System.out, true);
-    /* Similar to scanf in C */
+    // Similar to scanf in C
     Scanner inpScanner = new Scanner(System.in);
     pen.println("What is the expression you would like to evaluate?");
-    /* Open a scanner */
+    // Open a scanner
     String inpObj = inpScanner.nextLine();
-    /* Initialize the calculator from the factored out codes. */
+    // Initialize the calculator from the factored out codes.
     CalculatorFunctions calcFunc = new CalculatorFunctions(pen);
-    /* In case of QUIT or EXIT, the loop will end. */
+    // In case of QUIT or EXIT, the loop will end.
     while (!inpObj.equals("QUIT") && !inpObj.equals("EXIT")) {
       try {
         calcFunc.store(inpObj);
@@ -27,10 +27,10 @@ public class InteractiveCalculator {
         inpScanner.close();
         throw new Exception(e.getMessage(), e.getCause());
       } // try/catch
-      /* Reading the input from next line. */
+      // Reading the input from next line.
       inpObj = inpScanner.nextLine();
     } // while
-    /* Have to close the stream when opened */
+    // Have to close the stream when opened
     inpScanner.close();
     pen.close();
   } // main (String[]) throws Exception
